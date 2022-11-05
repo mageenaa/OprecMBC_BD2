e779f34008d45d36676e40089b40f4aa02c430b2
Devan Ramadhana <devanr2911@gmail.com>
2022-11-05 22:01:33 +0700

    update rumus totalbelanja

--- Nomor 2/nomor2.py
+++ Nomor 2/nomor2.py
@@ -4,12 +4,19 @@ member = int(input("Apakah anda mempunyai member?\nketik 1 jika anda mempunyai m
 diskon1 = int(totalbelanja-(totalbelanja*(5/100)))
 diskon2 = int(totalbelanja-(totalbelanja*(2/100)))
 diskon3 = int(totalbelanja-(totalbelanja*(3/100)))
+diskon4 = int(totalbelanja-(totalbelanja*(7/100)))
+diskon5 = int(totalbelanja-(totalbelanja*(8/100)))
 if member == 1:
-    print("Anda mendapat diskon sebesar 5% sehingga total belanjaan anda menjadi: Rp.",diskon1)
+    if totalbelanja >= 500000 and totalbelanja <= 1000000:
+        print("Anda mendapat diskon sebesar 7% sehingga total belanjaan anda menjadi: Rp.",diskon4)
+    elif totalbelanja > 1000000:
+        print("Anda mendapat diskon sebesar 8% sehingga total belanjaan anda menjadi: Rp.",diskon5)
+    else:
+        print("Anda mendapat diskon sebesar 5% sehingga total belanjaan anda menjadi: Rp.",diskon1)
 else:
-    if totalbelanja >= 500000:
+    if totalbelanja >= 500000 and totalbelanja <= 1000000:
         print("Anda mendapat diskon sebesar 2% sehingga total belanjaan anda menjadi: Rp.",diskon2)
-    elif totalbelanja >= 1000000:
+    elif totalbelanja > 1000000:
         print("Anda mendapat diskon sebesar 3% sehingga total belanjaan anda menjadi: Rp.",diskon3)
     else:
         print("Anda tidak mendapat diskon apapun")
